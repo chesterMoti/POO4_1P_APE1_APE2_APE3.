0129bbdf85d5a28872425885c0407aee5688ca19
Jackson Hidalgo <chester46077@gmail.com>
2026-06-23 14:58:24 +1300

    aniadiendo atributos en clase Usuario

---
+++ demo/src/main/java/poo/proyecto/Usuario.java
@@ -0,0 +1,12 @@
+package poo.proyecto;
+public class Usuario{
+    String codigoUnico;
+    String cedula;
+    String nombre;
+    String apellido;
+    String usuario;
+    String contraseña;
+    String correo;
+    
+}
+
