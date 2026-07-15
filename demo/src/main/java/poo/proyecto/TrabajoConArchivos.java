e5fd53e3534eb8b5e9200438f96ed43c3c6850e3
Jackson Hidalgo <chester46077@gmail.com>
2026-07-15 15:44:06 +1300

    Agregando clase TrabajandoConArchivos

---
+++ demo/src/main/java/poo/proyecto/TrabajoConArchivos.java
@@ -0,0 +1,16 @@
+package poo.proyecto;
+
+public class TrabajoConArchivos {
+    public static void main(String[] arg) {
+        ManejoArchivos.EscribirArchivo("archivosalida.txt","Hola!");
+        ManejoArchivos.EscribirArchivo("archivosalida.txt","Adios!");
+        ManejoArchivos.EscribirArchivo("archivosalida.txt","NUevo!");
+        ManejoArchivos.LeeFichero("archivosalida.txt");
+  
+        
+        
+    }
+
+}
+    
+
